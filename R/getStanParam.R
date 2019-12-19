fe931c1cd463f4ebaccddb15a6371dd872819c63
Olivier MacBook <olivier.granacher@gmail.com>
2019-12-19 20:37:58 +0100

    version 0.2.0

--- R/getStanParam.R
+++ R/getStanParam.R
@@ -1,16 +1,16 @@
-#' gets data_fame with mean and quantiles of Stan model parameters
+#' returns data_fame with mean and quantiles of Stan model parameters
 #'
-#'
+#' returns data_fame with mean and quantiles of Stan model parameters
 #'
 #' @param model  a Stan fit obtained with rstan::stan()
 #' @param param.name name (char) of parameter to get
-
 #'
 #' @return dataframe with 4 columns : parameter names, mean, 10 and 90% quantiles
 #'
 #' @export
 
 getStanParam <- function(model, param.name) {
+  post <- rstan::extract(model)
   param.post <- data.frame(post[param.name])
   param.mean <- purrr::map_dbl(param.post, mean)
   param.q10  <-  purrr::map_dbl(param.post, quantile, probs = .1)
