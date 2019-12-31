0cd79fb68069a1e110d2f72248929942ebfa98ee
Olivier MacBook <olivier.granacher@gmail.com>
2019-12-31 08:26:00 +0100

    0.3.4

--- R/addStanPred.R
+++ R/addStanPred.R
@@ -1,6 +1,6 @@
 #' Adds predictions and quantiles to data from Stan model
 #'
-#'
+#' Adds columns to dataframe containing model data
 #'
 #' @param model  a Stan fit obtained with rstan::stan()
 #' @param data dataframe with data used by the Stan model
@@ -8,7 +8,7 @@
 #' @param q1   low value for quantiles
 #' @param q2   high value for quantiles
 #'
-#' @return adds 3 columns to data : pred, q10 and q90
+#' @return adds 4 columns to data : pred mean, pred.median, predq10 and q90
 #'
 #' @export
 
@@ -16,6 +16,7 @@ addStanPred <- function(model, data, sim, q1 = .1, q2 = .9 ) {
   post <- rstan::extract(model)
   pred <- data.frame(post[as.character(sim)])
   data$pred <- purrr::map_dbl(pred, mean)
+  data$pred.median <- purrr::map_dbl(pred, median)
   data$q10  <- purrr::map_dbl(pred, quantile, probs = q1)
   data$q90  <- purrr::map_dbl(pred, quantile, probs = q2)
   return(data)
