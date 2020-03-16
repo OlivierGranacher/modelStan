b4686ed39a2c3de496ab6f67610542f936ec425b
Olivier MacBook <olivier.granacher@gmail.com>
2020-03-16 20:28:51 +0100

    v 0.7.1

--- R/plotStanParam.R
+++ R/plotStanParam.R
@@ -4,19 +4,21 @@
 #'
 #' @param model  a Stan fit obtained with rstan::stan()
 #' @param param.name name (char) of parameter to get
+#' @param names vector of names of groups in plot -
 #'
 #' @return ggplot with point range for each parameter
 #'
 #' @export
 
-plotStanParam <- function(model, param.name) {
+plotStanParam <- function(model, param.name, names = NULL) {
   post <- rstan::extract(model)
   param.post <- data.frame(post[param.name])
   param.mean <- purrr::map_dbl(param.post, mean)
   param.q10  <-  purrr::map_dbl(param.post, quantile, probs = .1)
   param.q90  <-  purrr::map_dbl(param.post, quantile, probs = .9)
   result <- data.frame(cbind(param.mean, param.q10, param.q90))
-  result <- cbind(names = names(param.post), result)
+  if (is.null(names)) names <- names(param.post)
+  result <- cbind(names = names, result)
   result$names <- forcats::fct_inorder(result$names)
   p <- ggplot(result) + geom_pointrange(aes(x = result[, 1],
                         y    = result[, 2],
